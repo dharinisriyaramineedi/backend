028f3d376e3bfd5f21e67f7fee3cd9422519c29c
dharinisriyaramineedi <127236866+dharinisriyaramineedi@users.noreply.github.com>
2023-04-21 14:39:15 +0530

    Add files via upload

---
+++ greetings.hello.js
@@ -0,0 +1,15 @@
+14 @@
+{
+    "questions": [
+        "hello",
+        "hi",
+        "howdy",
+        "Greetings"
+    ],
+    "answers": [
+        "Hey there!",
+        "Hello",
+        "Hi",
+        "Greetings"
+    ]
+}
